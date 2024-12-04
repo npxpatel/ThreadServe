7ff3d95a0c4ae0cafa83e9a7f497aad0e7980be7
npxpatel <nirajkumarpatel21@gmail.com>
2024-12-04 09:22:51 +0000

    LRU cache

---
+++ proxy_server_with_cache.c
@@ -0,0 +1,26 @@
+#include "proxy_parse.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <time.h>
+
+
+struct cache_element{
+    char *data;
+    int len;
+    char *url;
+    time_t lru_time;   // will remove when converting to hashmap + DLC
+    struct cache_element *next;
+};
