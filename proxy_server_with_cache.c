949e0f77b54229457b01de16d6a5b5b5b07d7d93
npxpatel <nirajkumarpatel21@gmail.com>
2024-12-07 14:38:45 +0000

    Added thread function

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -17,6 +17,7 @@
 #include <time.h>
 
 #define MAX_CLIENTS 5
+#define MAX_BYTES (1 << 20)    // 1 MB 
 
 struct cache_element{
     char *data;
@@ -41,7 +42,91 @@ pthread_mutex_t lock;
 struct cache_element *head;
 int cache_size;
 
+void *thread_func(void *NewSocket){
+    sem_wait(&semaphore);
+    int sem_value;
+    sem_getvalue(&semaphore, sem_value);
+    printf("Semaphore value is %d\n", sem_value);
 
+    int socket = *(int *)NewSocket;
+    int bytes_client_sends, len;
+
+    char *buffer = (char *)calloc(MAX_BYTES, sizeof(char));
+    bzero(buffer, MAX_BYTES);
+    bytes_client_sends = recv(socket, buffer, MAX_BYTES, 0);
+
+    while(bytes_client_sends > 0){
+        len = strlen(buffer);
+        if(strstr(buffer, "\r\n\r\n") == NULL){
+            bytes_client_sends = recv(socket, buffer + len, MAX_BYTES - len, 0);
+        }
+        else{
+            break;
+        }
+    }
+ 
+    char *tempReq = (char *)malloc(sizeof(buffer)*sizeof(char) + 1);
+    for(int i = 0; i < strlen(buffer); i ++){
+        tempReq[i] = buffer[i];
+    }
+  
+    struct cache_element *temp = find(tempReq);
+
+    if(temp != NULL){
+         // req is present in our Cache
+         int size = temp->len / sizeof(char);
+         int idx = 0;
+         char response[MAX_BYTES];
+         while(idx < size){
+            for(int i = 0; i < MAX_BYTES; i ++){
+                response[i] = temp->data[i];
+                idx ++;
+            }
+            send(socket, response, MAX_BYTES, 0);
+         }
+         printf("Response from the LRU Cache\n");
+         printf("Data : %s\n", response);
+    }
+    else if(bytes_client_sends > 0){
+        // find from the targeted server
+        int len = strlen(buffer);
+        ParsedRequest *request = ParsedRequest_create();
+
+        if(ParsedRequest_parse(request, buffer, len) < 0){
+            printf("Parsing failed\n");
+        }
+        else{
+            bzero(buffer, MAX_BYTES);
+            if(!strcmp(request->method,"GET")){
+               if(request->host && request->path && checkHTTPversion(request->version) == 1){
+                  bytes_client_sends = handle_request(socket, request, tempReq);
+                  if(bytes_client_sends == -1){
+                     sendErrMsg(socket, 500);
+                  }
+               }
+               else{
+                  sendErrMsg(socket, 500)
+               }
+            }
+            else {
+                print("Supports GET method only\n");
+            }
+        }
+        ParsedRequest_destroy(request);
+    }
+    else if(bytes_client_sends == 0){
+        printf("Client gone down !");
+    }
+    shutdown(socket, SHUT_RDWR);
+    close(socket);
+    free(buffer);
+    free(tempReq);
+    sem_post(&semaphore);
+    sem_getvalue(&semaphore, sem_value);
+    printf("Post semaphore value %d\n", sem_value);
+    
+    return NULL;
+}
 
 int main(int argc, char *argv[]){
     int client_socketId, client_len;
@@ -67,13 +152,16 @@ int main(int argc, char *argv[]){
     }
     int reuse = 1;  // global socket : proxy socket
     if(setsockopt(proxy_socketId, SOL_SOCKET, SO_REUSEADDR, (const char *)&reuse, sizeof(reuse)) < 0){
-        perror("SetSockOpt Failed");
+        perror("SetSockOpt Failed");  
     }
 
     bzero((char *)&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port_number);
-    server_addr.sin_addr.s_addr = INADDR_ANY;    // target server addr for now as any, 
+    server_addr.sin_addr.s_addr = INADDR_ANY;    
+
+     /* When binding a socket to an IP address, setting sin_addr.s_addr to INADDR_ANY tells the 
+     operating system to listen for incoming connections on any and all network interfaces on the host machine.  */
 
 
     if(bind(proxy_socketId, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
